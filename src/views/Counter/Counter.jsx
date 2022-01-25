adc87585c2df770172d1e780f4a3154857923479
JuanSierra <jksierra@gmail.com>
2022-01-25 09:39:03 +0000

    Add React Router

---
+++ src/views/Counter/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = props => {
+  return (
+    <div>
+      <h3>Counter View</h3>
+      <p> This is the Counter</p>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/views/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = props => {
+  return (
+    <div>
+      <h3>Main View</h3>
+      <p> This is the Main</p>
+    </div>
+  );
+};
+
+export default Main;
